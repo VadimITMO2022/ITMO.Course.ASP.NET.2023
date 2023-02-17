7718ce1c305d76baff132d8aa2199197b0151299
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-02-17 18:05:41 +0300

    Add files via upload

---
+++ ASP.NET/Lab1-3/RSVP/PartyInvites/Reg.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reg.aspx.cs" 
+    Inherits="PartyInvites.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="PartyStyles.css" />
+</head>
+<body>
+    <form id="rsvpform" runat="server">
+      <!--  <div>
+            <h1>New Year's Eve at Jacqui's!</h1>
+            <p>We're going to have an exciting party. And you're invited!</p>
+        </div>  -->
+
+        <div> <h1>Приглашаем на семинар</h1> <p></p> </div>
+
+        <asp:ValidationSummary ID="validationSummary" runat="server" ShowModelStateErrors="true" />
+        <div><label>Ваше имя:</label><input type="text" id="name" runat="server"/></div>
+        <div><label>Ваш email:</label><input type="text" id="email" runat="server" /></div>
+        <div><label>Ваш телефон:</label><input type="text" id="phone" runat="server" /></div>
+        <div>
+            <label>Вы будете делать доклад?</label>
+           <!-- <asp:CheckBox ID="CheckBoxYN" runat="server" /> -->
+            <select id="willattend" runat="server">
+                <option value="">Choose an Option</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+            </select> 
+            
+        </div>
+        <div>
+            <button type="submit">Отправить ответ на приглашение RSVP</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/Lab1-3/RSVP/PartyInvites/Reg.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace PartyInvites
+{
+
+
+    public partial class Default
+    {
+
+        /// <summary>
+        /// rsvpform элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm rsvpform;
+
+        /// <summary>
+        /// validationSummary элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary validationSummary;
+
+        /// <summary>
+        /// name элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText name;
+
+        /// <summary>
+        /// email элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText email;
+
+        /// <summary>
+        /// phone элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText phone;
+
+        /// <summary>
+        /// CheckBoxYN элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBoxYN;
+
+        /// <summary>
+        /// willattend элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически созданное поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect willattend;
+    }
+}
